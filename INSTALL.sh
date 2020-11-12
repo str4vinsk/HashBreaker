d42aba3b65707a906fe568cc62647b65912b0dd6
Vitor Conroy <hendrixisvolpt69@gmail.com>
2020-11-12 12:13:51 -0400

    Add files via upload

---
+++ INSTALL.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#__str4vinsk__
+
+
+function install()
+{
+	echo "Installing..."
+        chmod 777 hash.py
+        cp hash.py /usr/bin/hashbreaker
+	echo "Done! execute {hashbreaker} to use."
+	exit
+}
+
+if [ "$EUID" -ne 0 ];then
+	echo "[!!] Run the installer as root! [!!]"
+  	exit
+fi
+
+if [[ -e "/usr/bin/hashbreaker" ]]; then
+                echo "Hashbreaker is already installed!"
+		read -p "Do you want to reinstall it (y/n) ? " yn
+		if [[ "$yn" = "y" ]]; then
+			rm /usr/bin/hashbreaker
+                	install
+		elif [ "$yn" = "n" ]; then
+			echo "Okay"
+			exit
+		else
+			echo "[!!] Invalid option!"
+			exit
+		fi
+        else
+		install
+	fi
+else
+	install
+fi
